94c1c7894a1d525ba7f9ffd1c3e5a58ab04b8c22
lbrugnagodamaia <l.brugnago@hotmail.com>
2020-03-25 21:31:02 -0300

    break
    Adicionando o comando break.

--- src/contador01/Contador01.java
+++ src/contador01/Contador01.java
@@ -19,8 +19,11 @@ public class Contador01 {
         int cc = 0;
         while (cc < 10){
             cc++;
-            if (cc == 5 || cc == 7 || cc == 9){
+            if (cc == 2 || cc == 3 || cc == 4){
                 continue;
+            }
+            if (cc == 7){
+                break;
             }
             System.out.println("Cambalhota " + cc);
             
